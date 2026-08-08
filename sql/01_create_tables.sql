a1772d72856773eefaa9f09579e176e0b7cbca1a
Ashmizan <ashfiya.sharin.mizan@gmail.com>
2026-08-08 17:07:01 +0300

    Define tables for customers, furniture, and orders
    This script defines the structure and relationships between customers, furniture, and orders. It creates the customers, furniture, and orders tables with appropriate fields and foreign key constraints.

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,1 +1,36 @@
-Coming soon
+_ _ This script defines the structure and relationships between customers, 
+  furniture, and orders.
+
+  _ _ Create the customers table to store customer information.
+  CREATE TABLE customers (
+          customer_id INTEGER PRIMARY KEY,
+          customer_name VARCHAR(100),
+          country VARCHAR(100)
+);
+
+_ _ Create the furniture table to store product information.
+CREATE TABLE furniture (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR (100),
+    category VARCHAR (100),
+    color VARCHAR (50),
+    price INTEGER
+);
+
+_ _ Create the orders table to store
+  customer purchases. 
+ CREATE TABLE orders (
+     order_id INTEGER PRIMARY KEY,
+     customer_id INTEGER,
+     furniture_id INTEGER,
+     quantity INTEGER,
+
+    FOREIGN KEY (customer_id)
+        REFERENCES
+   customers(customer_id),
+
+    FOREIGN KEY (furniture_id)
+         REFERENCES furniture(id)
+   );
+
+
